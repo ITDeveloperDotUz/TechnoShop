a6cd40c9107ff6bebebd50643ad97088c6cad4b1
ITDeveloperDotUz <48922487+ITDeveloperDotUz@users.noreply.github.com>
2019-06-10 19:52:54 +0500

    redesigning process

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,41 +3,46 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <table class="table table-striped bg-light">
-                <thead>
-                <tr>
-                    <td>Махсулот номланиши</td>
-                    <td>Махсулот тури</td>
-                    <td>Махсулот маълумотлари</td>
-                    <td>Махсулот нархи</td>
-                    <td>
-                        <a class="btn btn-success float-right" href="{{ route('products.create') }}"><i class="fa fa-plus"></i></a>
-                    </td>
-                    <td></td>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($products as $product)
-                <tr>
-                    <td>
-                        {{ $product->name }}
-                    </td>
-                    <td>{{ $product->category->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->price }}</td>
+            <div class="card col-12 text-white bg-light">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <table class="table table-condensed bg-none">
+                        <thead>
+                        <tr>
+                            <td>Махсулот номланиши</td>
+                            <td>Махсулот тури</td>
+                            <td>Махсулот маълумотлари</td>
+                            <td>Махсулот нархи</td>
+                            <td>
+                                <a class="btn btn-success float-right" href="{{ route('products.create') }}"><i class="fa fa-plus"></i></a>
+                            </td>
+                            <td></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($products as $product)
+                            <tr>
+                                <td>
+                                    {{ $product->name }}
+                                </td>
+                                <td>{{ $product->category->name }}</td>
+                                <td>{{ $product->description }}</td>
+                                <td>{{ $product->price }}</td>
 
-                    <td><a class="btn btn-info float-right" href="{{ route('products.edit', [$product->id]) }}"><i class="fa fa-edit"></i></a></td>
-                    <td>
-                        <form class="pull-right" method="post" action="{{ route('products.destroy', [$product->id]) }}">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-danger"><i class="fa fa-trash-alt"></i></button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-                </tbody>
-            </table>
+                                <td><a class="btn btn-info float-right" href="{{ route('products.edit', [$product->id]) }}"><i class="fa fa-edit"></i></a></td>
+                                <td>
+                                    <form class="pull-right" method="post" action="{{ route('products.destroy', [$product->id]) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger"><i class="fa fa-trash-alt"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
